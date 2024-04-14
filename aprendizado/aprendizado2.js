799000a60c3ac26998062cbeda94fa91e58c2af0
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-04-14 19:13:09 -0300

    Organizaçao de pastas

---
+++ aprendizado/aprendizado2.js
@@ -0,0 +1,11 @@
+var numb = 12; // Variável que armazena um número inteiro
+var name = 'ELTO'; // Variável que armazena uma string (texto)
+var bool = true; // Variável que armazena um valor booleano (verdadeiro ou falso)
+var nula = null; // Variável que armazena um valor nulo
+var undefinedVar; // Variável que não foi atribuída a nenhum valor
+
+console.log(typeof numb); // Imprime o tipo de dado da variável numb
+console.log(typeof name); // Imprime o tipo de dado da variável name
+console.log(typeof bool); // Imprime o tipo de dado da variável bool
+console.log(typeof nula); // Imprime o tipo de dado da variável nula
+console.log(typeof undefinedVar); // Imprime o tipo de dado da variável undefinedVar
--- exercicios Modulo B/aprendizado2.js
+++
@@ -1,7 +0,0 @@
-var numb = 12; // Variável que armazena um número inteiro
-var name = 'ELTO'; // Variável que armazena uma string (texto)
-var bool = true; // Variável que armazena um valor booleano (verdadeiro ou falso)
-
-console.log(typeof numb); // Imprime o tipo de dado da variável numb
-console.log(typeof name); // Imprime o tipo de dado da variável name
-console.log(typeof bool); // Imprime o tipo de dado da variável bool
