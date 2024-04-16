8be3cef98d6f7beeb0d4d7c49afc2e0bfd92f3fb
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-04-16 02:13:02 -0300

    Atualização do exercicio 4 e modificando o aprendizado 4

--- aprendizado/aprendizado4.js
+++ aprendizado/aprendizado4.js
@@ -5,11 +5,11 @@ let produto = 2 * 4; // (produto será igual a 8)
 let quociente = 10 / 2; // (quociente será igual a 5)
 let resto = 10 % 3; // (resto será igual a 1)
 
-console.log(soma); // Imprime a soma no console
-console.log(diferenca); // Imprime a diferença no console
-console.log(produto); // Imprime o produto no console
-console.log(quociente); // Imprime o quociente no console
-console.log(resto); // Imprime o resto no console
+console.log("A soma é:", soma); // Imprime a soma no console
+console.log("A diferença é:", diferenca); // Imprime a diferença no console
+console.log("O produto é:", produto); // Imprime o produto no console
+console.log("O quociente é:", quociente); // Imprime o quociente no console
+console.log("O resto é:", resto); // Imprime o resto no console
 
 // Operadores de Atribuição:
 let x = 5; // (x será igual a 5)
@@ -26,21 +26,21 @@ let menor = 3 < 5; // (menor será igual a true)
 let maiorOuIgual = 5 >= 5; // (maiorOuIgual será igual a true)
 let menorOuIgual = 3 <= 5; // (menorOuIgual será igual a true)
 
-console.log(igual); // Imprime o valor de igual no console
-console.log(diferente); // Imprime o valor de diferente no console
-console.log(maior); // Imprime o valor de maior no console
-console.log(menor); // Imprime o valor de menor no console
-console.log(maiorOuIgual); // Imprime o valor de maiorOuIgual no console
-console.log(menorOuIgual); // Imprime o valor de menorOuIgual no console
+console.log("5 igual a 5?", igual); // Imprime o valor de igual no console
+console.log("5 diferente de 3?", diferente); // Imprime o valor de diferente no console
+console.log("10 maior que 5?", maior); // Imprime o valor de maior no console
+console.log("3 menor que 5?", menor); // Imprime o valor de menor no console
+console.log("5 maior ou igual a 5?", maiorOuIgual); // Imprime o valor de maiorOuIgual no console
+console.log("3 menor ou igual a 5?", menorOuIgual); // Imprime o valor de menorOuIgual no console
 
 // Operadores Lógicos:
 let and = true && true; // (and será igual a true)
 let or = true || false; // (or será igual a true)
 let not = !true; // (not será igual a false)
 
-console.log(and); // Imprime o valor de and no console
-console.log(or); // Imprime o valor de or no console
-console.log(not); // Imprime o valor de not no console
+console.log("and:", and); // Imprime o valor de and no console
+console.log("or:", or); // Imprime o valor de or no console
+console.log("not:", not); // Imprime o valor de not no console
 
 
 // Operadores de Incremento e Decremento:
@@ -49,29 +49,30 @@ i++; // (i será igual a 6)
 let j = 5;
 j--; // (j será igual a 4)
 
-console.log(i); // Imprime o valor de i no console
-console.log(j); // Imprime o valor de j no console
+console.log("i:", i); // Imprime o valor de i no console
+console.log("j:", j); // Imprime o valor de j no console
 
 // Operador Ternário:
 let idade = 18;
 let status = (idade >= 18) ? 'Maior de idade' : 'Menor de idade'; // (status será igual a 'Maior de idade')
-console.log(status); // Imprime o valor de status no console
+console.log("status:", status); // Imprime o valor de status no console
 
 // Operador de Concatenação:
 let nome = 'João';
 let sobrenome = 'Silva';
 let nomeCompleto = nome + ' ' + sobrenome; // (nomeCompleto será igual a 'João Silva')
-
-console.log(nomeCompleto); // Imprime o valor de nomeCompleto no console
+console.log("nome", nome); // Imprime o valor de nome no console
+console.log("sobrenome:", sobrenome); // Imprime o valor de sobrenome no console
+console.log("nomeCompleto:", nomeCompleto); // Imprime o valor de nomeCompleto no console
 
 // Operador de Tipo:
 let tipo = typeof 5; // (tipo será igual a 'number')
 let tipoString = typeof 'JavaScript'; // (tipoString será igual a 'string')
 let tipoBoolean = typeof true; // (tipoBoolean será igual a 'boolean')
 
-console.log(tipo); // Imprime o valor de tipo no console
-console.log(tipoString); // Imprime o valor de tipoString no console
-console.log(tipoBoolean); // Imprime o valor de tipoBoolean no console
+console.log("tipo:", tipo); // Imprime o valor de tipo no console
+console.log("tipoString:", tipoString); // Imprime o valor de tipoString no console
+console.log("tipoBoolean:", tipoBoolean); // Imprime o valor de tipoBoolean no console
 
 /*Em JavaScript, a ordem de precedência dos operadores é a seguinte:
 
